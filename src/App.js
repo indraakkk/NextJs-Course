66d48e87830cdea67417043a9435a5a23401c8ad
dev <dev@mail.com>
2022-10-13 15:34:44 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="learn react" />
+      <Todo text="Master react" />
+      <Todo text="Explore react" />
+    </div>
+  );
+}
+
+export default App;
