5bbe1625aca80bab720327c8ca04f3273b5dc2be
indraakkk <indrakoslab@gmail.com>
2022-10-13 15:51:06 +0700

    cra minimal template

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import Todo from "./components/Todo";
 
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="learn react" />
-      <Todo text="Master react" />
-      <Todo text="Explore react" />
-    </div>
-  );
+  return <div>Hello!</div>;
 }
 
 export default App;
