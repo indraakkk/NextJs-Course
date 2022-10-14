f4c813b1763085aebe3e534de7bdca791addc038
indraakkk <indrakoslab@gmail.com>
2022-10-14 12:12:52 +0700

    implement Layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritePage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetups";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
         <Route path="/favorites" element={<FavoritePage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
