44fa362f609b96b5ca847b12e26ce744707a6250
indraakkk <indrakoslab@gmail.com>
2022-10-14 11:16:26 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritePage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetups";
@@ -6,6 +7,7 @@ import NewMeetupPage from "./pages/NewMeetups";
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
