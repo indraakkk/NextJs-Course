da5b4b949b07b739d456bb3e69454e1b8a170e42
indraakkk <indrakoslab@gmail.com>
2024-01-23 16:11:48 +0700

    feat: :sparkles: implement slug and closest notFound custom page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,39 @@
-export default function MealPage({ params }) {
+import Image from 'next/image';
+
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
+
+export default function MealDetailsPage({ params }) {
   const { slug } = params;
+  const meal = getMeal(slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
   return (
-    <main>
-      <h1>Meal</h1>
-      <p>{slug}</p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,16 +1,38 @@
+import { Suspense } from 'react';
 import Link from 'next/link';
+import MealsGrid from '@/components/meals/meal-grid';
+import LoadingMeals from './loading-meals';
+import { getMeals } from '@/lib/meals';
 
-export default function MealsPage() {
+import classes from './page.module.css';
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
+  const meals = await getMeals();
   return (
-    <main>
-      <h1>Blog Page</h1>
-
-      <p>
-        <Link href="/meals/burger">New Blog</Link>
-      </p>
-      <p>
-        <Link href="/meals/not-burger">News!</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<LoadingMeals />}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   );
 }
